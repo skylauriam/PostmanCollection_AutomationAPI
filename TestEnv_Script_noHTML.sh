0c8ceabbc1d7adb7f8932cde86910e974fad5a65
Christian Catelli <christian.catelli.kineton@skytv.it>
2020-02-20 17:20:55 +0100

    Adding updated uat&test file. Collection updated

--- TestEnv_Script_noHTML.sh
+++ TestEnv_Script_noHTML.sh
@@ -1,1 +1,4 @@
-newman run /Users/christian.catelli/GoogleDrive\(christian.catelli@kineton.it\)/Sky/Automation_IT_CI_CD_WSC/Repository/PostmanCollection_AutomationAPI/Automation.postman_collection.json -e /Users/christian.catelli/GoogleDrive\(christian.catelli@kineton.it\)/Sky/Automation_IT_CI_CD_WSC/Repository/PostmanCollection_AutomationAPI/Test.postman_environment.json -d /Users/christian.catelli/GoogleDrive\(christian.catelli@kineton.it\)/Sky/Automation_IT_CI_CD_WSC/Repository/PostmanCollection_AutomationAPI/Test.data_file.json           
+at now + 3 mins
+echo "\nHI! Starting postman_collection\n"
+cd GoogleDrive\(christian.catelli@kineton.it\)/Sky/Automation_IT_CI_CD_WSC/Repository/PostmanCollection_AutomationAPI/
+newman run Automation.postman_collection.json -e Test.postman_environment.json -d Test.data_file.json && exit
--- cronojob.sh
+++ cronojob.sh
@@ -1,1 +1,1 @@
-*/2 * * * * /Users/christian.catelli/GoogleDrive\(christian.catelli@kineton.it\)/Sky/Automation_IT_CI_CD_WSC/Repository/PostmanCollection_AutomationAPI/script.sh
+*/3 * * * * /Users/christian.catelli/GoogleDrive\(christian.catelli@kineton.it\)/Sky/Automation_IT_CI_CD_WSC/Repository/PostmanCollection_AutomationAPI/script.sh
