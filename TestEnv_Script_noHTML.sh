755e0f51627f2110eba6f83f61cc04b4c2147c10
Christian Catelli <christian.catelli.kineton@skytv.it>
2020-03-04 13:32:23 +0100

    Custom workflow is now achieved through environment variable and not external data file. Updated environment files

--- TestEnv_Script_noHTML.sh
+++
@@ -1,4 +0,0 @@
-at now + 3 mins
-echo "\nHI! Starting postman_collection\n"
-cd GoogleDrive\(christian.catelli@kineton.it\)/Sky/Automation_IT_CI_CD_WSC/Repository/PostmanCollection_AutomationAPI/
-newman run Automation.postman_collection.json -e Test.postman_environment.json -d Test.data_file.json && exit
